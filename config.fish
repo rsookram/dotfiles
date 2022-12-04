8c71f9477e34453dcdeebda932da41669d34c3e4
rsookram <rashadsookram@gmail.com>
2022-12-03 19:51:15 -0500

    Stop using spectrwm and start using brew on Linux

--- config.fish
+++ config.fish
@@ -121,6 +121,13 @@ set -gx PATH $PATH ~/tools/depot_tools
 
 if test (uname) = "Darwin"
   set -gx PATH $PATH /opt/homebrew/bin
+else
+  set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+  set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+  set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+  set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+  set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+  set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
 end
 
 # Go installation is managed by homebrew on macos
