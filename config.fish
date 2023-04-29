66b72077fdf655b969984fa84dc4ece213e926d0
rsookram <rashadsookram@gmail.com>
2023-04-28 22:35:30 -0400

    Manage rust-analyzer through rustup

--- config.fish
+++ config.fish
@@ -124,7 +124,7 @@ else
   set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
   set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
   set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
-  set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+  set -gx PATH $PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
   set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
   set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
 end
