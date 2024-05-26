da20f7e0bc96cb7148216f5fe94cb6fb667051b6
rsookram <rashadsookram@gmail.com>
2024-05-26 17:17:09 -0400

    Update use of fd
    .git/ is no longer excluded by default when --hidden is passed.

--- init.lua
+++ init.lua
@@ -81,7 +81,7 @@ require("telescope").setup{
   pickers = {
     find_files = {
       -- Hidden files aren't shown by default
-      find_command = { "fd", "--hidden", "--type", "f", "--strip-cwd-prefix", "--glob", "" },
+      find_command = { "fd", "--hidden", "--type", "f", "--strip-cwd-prefix", "--glob", "", "--exclude", ".git/" },
     },
     live_grep = {
       path_display = { "shorten" },
