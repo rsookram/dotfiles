0ad84c47f5692b4474342773d0665e31f70454b7
rsookram <rashadsookram@gmail.com>
2024-08-29 22:03:45 -0400

    Open file at last position

--- init.lua
+++ init.lua
@@ -295,6 +295,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- jump to last edit position on opening file
+vim.api.nvim_create_autocmd(
+	'BufReadPost',
+	{
+		pattern = '*',
+		callback = function(ev)
+			if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+				-- except for in git commit messages
+				-- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
+				if not vim.fn.expand('%:p'):find('.git', 1, true) then
+					vim.cmd('exe "normal! g\'\\""')
+				end
+			end
+		end
+	}
+)
 
 -- Key bindings
 
